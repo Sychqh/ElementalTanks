be7cf5fda3bc019484817ee7387bbf7e14e45680
Sychqh <ilyasychkov@gmail.com>
2021-05-18 20:33:19 +0500

    Added first movement system, Tank class

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace ElementalTanks
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1280, 1024);
+            this.Name = "Form1";
+            this.Text = "Elemental Tanks";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,79 @@ namespace ElementalTanks
 {
     public partial class Form1 : Form
     {
+        private readonly Tank player = new Tank();
+        private readonly List<Tank> enemies;
+        private int score;
+        private Timer gameTimer;
+        bool rot;
+
+        private bool goLeft, goRight, goUp, goDown;
+
         public Form1()
         {
             InitializeComponent();
+            gameTimer = new Timer
+            {
+                Enabled = true,
+                Interval = 20,
+            };
+            gameTimer.Tick += MainTimerEvent;
+            gameTimer.Start();
+
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            Controls.Add(player.Sprite);
         }
 
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    goLeft = false;
+                    break;
+                case Keys.Right:
+                    goRight = false;
+                    break;
+                case Keys.Down:
+                    goDown = false;
+                    break;
+                case Keys.Up:
+                    goUp = false;
+                    break;
+            }
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (goLeft)
+                player.Sprite.Left -= 20;
+            if (goRight)
+                player.Sprite.Left += 20;
+            if (goUp)
+                player.Sprite.Top -= 20;
+            if (goDown)
+                player.Sprite.Top += 20;
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    goLeft = true;
+                    break;
+                case Keys.Right:
+                    goRight = true;
+                    break;
+                case Keys.Down:
+                    goDown = true;
+                    break;
+                case Keys.Up:
+                    goUp = true;
+                    break;
+            }
+        }
     }
 }
